005c7b7f755e56b70187100a33ffc4f64741db45
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2023-02-12 21:18:00 +0200

    6000

--- client/webrtc.js
+++ client/webrtc.js
@@ -9,7 +9,7 @@ var connectionState;
 var name; 
 var connectedUser;
 
-connection = new WebSocket('wss://' + 'localhost' + ':5000');
+connection = new WebSocket('wss://' + 'localhost' + ':6000');
 
 var Send_dataChannel, peerConnection, connectedUser, Receive_dataChannel;
 var username;

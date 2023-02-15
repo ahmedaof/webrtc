6ad91e94c65b4b09e923fb416f9c47105fd83e6b
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2023-02-15 19:38:50 +0200

    df

--- client/webrtc.js
+++ client/webrtc.js
@@ -9,7 +9,7 @@ var connectionState;
 var name; 
 var connectedUser;
 
- connection = new WebSocket(`wss://64.225.98.246:5000`);
+ connection = new WebSocket(`wss://${window.location.hostname}:4430`);
 
 
 var Send_dataChannel, peerConnection, connectedUser, Receive_dataChannel;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const HTTPS_PORT = 5000;
+const HTTPS_PORT = 4430;
 
 const fs = require('fs');
 const https = require('https');

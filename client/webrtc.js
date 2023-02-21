26d55e5e0520bf5af2a558b46c9bda0a94f76189
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2023-02-22 00:39:11 +0200

    df

--- client/webrtc.js
+++ client/webrtc.js
@@ -31,12 +31,12 @@ var id_wordflick;
 var configuration = {
     "iceServers": [
         {
-            "urls": "stun:stun.1.google.com:19302"
+            "urls": "stun:stun.ourcodeworld.com:5349"
         },
         {
-            urls: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
+            urls: 'turn:turn.ourcodeworld.com:5349',
+            credential: '12345',
+            username: 'brucewayne'
         }
     ]
 };

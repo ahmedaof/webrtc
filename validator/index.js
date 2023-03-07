35728652010367eab8db9212214c9d54fe5854e8
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2023-03-07 09:39:02 +0200

    number

--- validator/index.js
+++ validator/index.js
@@ -1,13 +1,6 @@
 exports.userSignupValidator = (req, res, next) => {
-    console.log('userSignupValidator    req.bodyddddddddddddddddddddddddddddddd');
     req.check('name', 'Name is required').notEmpty();
     req.check('email', 'Email must be between 3 to 32 characters')
-        .matches(/.+\@.+\..+/)
-        .withMessage('Email must contain @')
-        .isLength({
-            min: 4,
-            max: 32
-        });
     req.check('password', 'Password is required').notEmpty();
     req.check('password')
         .isLength({ min: 6 })

3cf79e7dc1d0b7c5a9a819b664ec8de8b174eb4f
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2023-03-21 09:35:28 +0200

    stun

--- client/webrtc.js
+++ client/webrtc.js
@@ -32,11 +32,6 @@ var configuration = {
     "iceServers": [
         {
             "urls": "stun:stun.1.google.com:19302"
-        },
-        {
-            urls: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
         }
     ]
 };

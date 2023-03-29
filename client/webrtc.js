cfd4b96859cbfd9e6adf939e2b2a629e4059e432
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2023-03-30 00:56:35 +0200

    testoffer

--- client/webrtc.js
+++ client/webrtc.js
@@ -331,23 +331,7 @@ function Create_DataChannel(name) {
 /**
  * This function will create the webRTC offer request for other user.
  */
- function creating_offer() {
-    document.getElementById('dynamic_progress_text').setAttribute('data-loading-text', "Requesting with user .. Please wait..");
-    try {
-        const offer =  peerConnection.createOffer({iceRestart:true});
-         peerConnection.setLocalDescription(offer);
 
-      
-        send({
-            type: "offer",
-            offer: offer
-        });
-
-    } catch (e) {
-        clear_outgoing_modal_popup(); /*remove modal when any error occurs */
-        alert("Failed to create offer:" + e);
-    }
-}
 /**
  * This function will send webRTC answer to server for offer request.
  */
@@ -863,9 +847,10 @@ function call_user(name,type) {
     var signallingState2 = yourConn.signalingState;
 
 
- 
-
+    
+    
     yourConn.createOffer(function (offer) { 
+        offer.sdp = preferOpus(offer.sdp);
        send({
           type: "offer", 
           offer: offer ,
@@ -1470,3 +1455,79 @@ fetch(`https://${window.location.hostname}:3000/auth/signout`, requestOptions)
   .catch(error => console.log('error', error));
 }
 
+
+
+ // Set Opus as the default audio codec if it's present.
+ function preferOpus(sdp) {
+    var sdpLines = sdp.split('\r\n');
+    var mLineIndex;
+    // Search for m line.
+    for (var i = 0; i < sdpLines.length; i++) {
+        if (sdpLines[i].search('m=audio') !== -1) {
+            mLineIndex = i;
+            break;
+        }
+    }
+    if (mLineIndex === null || mLineIndex === undefined) {
+        return sdp;
+    }
+
+    // If Opus is available, set it as the default in m line.
+    for (i = 0; i < sdpLines.length; i++) {
+        if (sdpLines[i].search('opus/48000') !== -1) {
+            var opusPayload = extractSdp(sdpLines[i], /:(\d+) opus\/48000/i);
+            if (opusPayload) {
+                sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], opusPayload);
+            }
+            break;
+        }
+    }
+
+    // Remove CN in m line and sdp.
+    sdpLines = removeCN(sdpLines, mLineIndex);
+
+    sdp = sdpLines.join('\r\n');
+    return sdp;
+}
+
+function extractSdp(sdpLine, pattern) {
+    var result = sdpLine.match(pattern);
+    return result && result.length === 2 ? result[1] : null;
+}
+
+// Set the selected codec to the first in m line.
+function setDefaultCodec(mLine, payload) {
+    var elements = mLine.split(' ');
+    var newLine = [];
+    var index = 0;
+    for (var i = 0; i < elements.length; i++) {
+        if (index === 3) { // Format of media starts from the fourth.
+            newLine[index++] = payload; // Put target payload to the first.
+        }
+        if (elements[i] !== payload) {
+            newLine[index++] = elements[i];
+        }
+    }
+    return newLine.join(' ');
+}
+
+// Strip CN from sdp before CN constraints is ready.
+function removeCN(sdpLines, mLineIndex) {
+    var mLineElements = sdpLines[mLineIndex].split(' ');
+    // Scan from end for the convenience of removing an item.
+    for (var i = sdpLines.length - 1; i >= 0; i--) {
+        var payload = extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
+        if (payload) {
+            var cnPos = mLineElements.indexOf(payload);
+            if (cnPos !== -1) {
+                // Remove CN payload from m line.
+                mLineElements.splice(cnPos, 1);
+            }
+            // Remove CN line in sdp
+            sdpLines.splice(i, 1);
+        }
+    }
+
+    sdpLines[mLineIndex] = mLineElements.join(' ');
+    return sdpLines;
+}
